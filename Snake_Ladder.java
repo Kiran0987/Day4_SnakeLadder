525f42cc97c001b87bb8cc26b1bab9af110975bd
kiran <sharmakiran237@gmail.com>
2022-05-10 23:10:23 +0530

    Total_No_Dice_Rolled

--- Snake_Ladder.java
+++ Snake_Ladder.java
@@ -44,8 +44,11 @@ public class Snake_Ladder {
 				break;
 			}	
 		}
+		System.out.println("Total number of times Dice rolled: "+diceRollCount);
+
 		if (position == 100) {
 			System.out.println(" Congratulations, you won!! ");	
 		}
+
 	}
 }
